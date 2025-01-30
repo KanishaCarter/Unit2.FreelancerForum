4ac090170d7937251d38112674ed43d238b1c9b6
Kanisha S Carter <235420@carmax.com>
2025-01-30 16:56:00 -0600

    BLOCK19 Workshop - Freelancer Forum

---
+++ script.js
@@ -0,0 +1,9 @@
+// Array of possible names
+const names = ["Alice", "Bob", "Carol", "David", "Eve", "Frank", "Grace", "Hank"];
+const occupations = ["Writer", "Teacher", "Programmer", "Designer", "Artist", "Driver"];
+
+// Array with two freelancers to start
+const freelancers = [
+    { name: "Alice", occupation: "Writer", price: 30 },
+    { name: "Bob", occupation: "Teacher", price: 50 },
+  ];
